08b3087d2076c0c191d2dfa866c17a47179eba79
Matthias Nüßler <m.nuessler@web.de>
2015-07-31 23:44:54 +0200

    InfluxDB 0.9.x support for influxdb-check
    Update check-influxdb.rb for InfluxDB 0.9. The API of the /ping check
    has been modified in InfluxDB 0.9 to return an empty body and a status
    of "204 No Content" on success.
    
    The influxdb-query-check does not need modification. It just requires a
    recent ruby client with InfluxDB 0.9 support.

--- bin/check-influxdb.rb
+++ bin/check-influxdb.rb
@@ -57,7 +57,7 @@ class CheckInfluxDB < Sensu::Plugin::Check::CLI
          default: false
 
   option :timeout,
-         description:            'Seconds to wait for the connection to open or read (default: 1.0s)',
+         description: 'Seconds to wait for the connection to open or read (default: 1.0s)',
          short: '-t SECONDS',
          long: '--timeout SECONDS',
          proc: proc(&:to_f),
@@ -69,11 +69,12 @@ class CheckInfluxDB < Sensu::Plugin::Check::CLI
     http.read_timeout = config[:timeout]
     http.use_ssl = config[:ssl]
     http.start do
-      status = JSON.parse(http.get('/ping').body)
-      if status == { 'status' => 'ok' }
-        ok status.to_s
+      response = http.get('/ping')
+      status_line = "#{response.code} #{response.message}"
+      if response.is_a?(Net::HTTPSuccess)
+        ok status_line
       else
-        critical status.to_s
+        critical status_line
       end
     end
   rescue => e
